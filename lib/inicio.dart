02a5d2f56782519822859b13ed9a3f4f7ef3ec71
Quetzal <flores.agustin.1am@gmail.com>
2023-12-04 13:04:40 -0600

    InicioUpdate
    El inicio we

--- lib/inicio.dart
+++ lib/inicio.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class InicioView extends StatefulWidget {
-  const InicioView({super.key});
+  const InicioView({Key? key}) : super(key: key);
 
   @override
   _InicioViewState createState() => _InicioViewState();
@@ -11,12 +11,33 @@ class _InicioViewState extends State<InicioView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
+      body: Column(children: [
+        Center(
+          child: Image.asset(
+            '/imagenes/BannerInicio.png',
+            width: double.infinity,
+            height: 150,
+            fit: BoxFit.cover,
           ),
-        ]
-      ),
+        ),
+        ListTile(
+          title: Text(
+            'Marcas',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+        ),
+        Center(
+          child: Image.asset(
+            '/imagenes/Marcas.png',
+            width: double.infinity,
+            height: 120,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ]),
     );
   }
 }
