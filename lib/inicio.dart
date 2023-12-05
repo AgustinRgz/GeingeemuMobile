f32a9eb65c0dd5c83f08eb9e074d0163dec9bb2a
Quetzal <flores.agustin.1am@gmail.com>
2023-12-05 16:19:01 -0600

    Update Inicio

--- lib/inicio.dart
+++ lib/inicio.dart
@@ -8,6 +8,33 @@ class InicioView extends StatefulWidget {
 }
 
 class _InicioViewState extends State<InicioView> {
+  Widget _buildImageWithText(String imagePath, String text, String textP) {
+    return Column(
+      children: [
+        Image.asset(
+          imagePath,
+          height: 150.0,
+        ),
+        SizedBox(height: 8.0),
+        Text(
+          text,
+          style: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          textP,
+          style: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(242, 35, 255, 1),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,19 +76,46 @@ class _InicioViewState extends State<InicioView> {
             ),
           ),
           Container(
-            height: 200.0,
+            height: 250.0,
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: <Widget>[
-                Image.asset('assets/imagenes/halo4.jpg'),
-                Image.asset('assets/imagenes/halo5.jpg'),
-                Image.asset('assets/imagenes/haloinfinite.jpg'),
-                Image.asset('assets/imagenes/halo4.jpg'),
-                Image.asset('assets/imagenes/halo5.jpg'),
-                Image.asset('assets/imagenes/haloinfinite.jpg'),
-                Image.asset('assets/imagenes/halo4.jpg'),
-                Image.asset('assets/imagenes/halo5.jpg'),
-                Image.asset('assets/imagenes/haloinfinite.jpg'),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/halo4.jpg', 'Halo 4', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/halo5.jpg', 'Halo 5', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/haloinfinite.jpg', 'Halo Infinite', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/kirby.jpg', 'Kirby and the forgotten lands', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/zelda.jpg', 'Zelda: TOTK', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/pokemon.jpg', 'Pokemon Scarlet', '\$1000.00'),
+                ),
               ],
             ),
           ),
@@ -75,19 +129,46 @@ class _InicioViewState extends State<InicioView> {
             ),
           ),
           Container(
-            height: 200.0,
+            height: 250.0,
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: <Widget>[
-                Image.asset('assets/imagenes/kirby.jpg'),
-                Image.asset('assets/imagenes/pokemon.jpg'),
-                Image.asset('assets/imagenes/zelda.jpg'),
-                Image.asset('assets/imagenes/kirby.jpg'),
-                Image.asset('assets/imagenes/pokemon.jpg'),
-                Image.asset('assets/imagenes/zelda.jpg'),
-                Image.asset('assets/imagenes/kirby.jpg'),
-                Image.asset('assets/imagenes/pokemon.jpg'),
-                Image.asset('assets/imagenes/zelda.jpg'),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/kirby.jpg', 'Kirby and the forgotten lands', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/zelda.jpg', 'Zelda: TOTK', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/pokemon.jpg', 'Pokemon Scarlet', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/halo4.jpg', 'Halo 4', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/halo5.jpg', 'Halo 5', '\$1000.00'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 16.0), // Adjust the spacing between images
+                  child: _buildImageWithText(
+                      'assets/imagenes/haloinfinite.jpg', 'Halo Infinite', '\$1000.00'),
+                ),
               ],
             ),
           ),
